ed63f8cee7491a93b04ca9f8cdcaba50294cca26
Nick Muller <muller_nicky@hotmail.com>
2017-11-12 15:47:14 +0100

    Bug

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyContext())
+            {
+                context.Database.EnsureCreated();
+
+                var entities = context.Set<MyEntity>().ToListAsync().Result;
+            }
+        }
+    }
+
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("QueryFilterBug");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            int tenantId = 123;
+            modelBuilder.Entity<MyEntity>()
+                .HasQueryFilter(e => e.TenantId == tenantId);
+        }
+    }
+
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public int TenantId { get; set; }
+    }
+}
